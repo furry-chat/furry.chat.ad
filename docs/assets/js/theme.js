d754725d8dba29c28631216f90aa34d4b17771a6
Kitsune Solar <kitsune.solar@gmail.com>
2019-04-03 13:23:45 +0300

    2019-04-03 10:23:44

---
+++ docs/assets/js/theme.js
@@ -0,0 +1,10 @@
+'use strict';
+
+discordEveryoneMessages.run(
+	'303327594793795588',
+	'383146754129920000',
+	'10',
+	'TlRRd01URTJOakV6TWpNeU16STROekEwLkR6TU9jUS4weFRCQVpiekFVcUtxRjNiRVJ4OHJlaDRRUWc=',
+	'feed',
+	'#data-ad'
+);
