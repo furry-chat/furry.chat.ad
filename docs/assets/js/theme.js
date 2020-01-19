4fec01b1dee06eab1bc679b67906655817ae9803
KitsuneSolar <kitsune.solar@gmail.com>
2020-01-19 21:46:35 +0300

    2020-01-19 18:46:35

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -5,7 +5,7 @@ discordEveryoneMessages.run(
 	'383146754129920000',
 	'10',
 	//'TlRRd01URTJOakV6TWpNeU16STROekEwLkR6TU9jUS4weFRCQVpiekFVcUtxRjNiRVJ4OHJlaDRRUWc=',
-  'TmpZNE5EazFOekF6TkRZeU5qUXhOamd6LlhpU1BrZy5hejRkZnd1dEdKa05RQk14c2tnTTVXZ25UN2M=',
+  'TmpZNE5EazFOekF6TkRZeU5qUXhOamd6LlhpU2piZy5Pc19zMml3Zy10SThFQTZ1Z1hld1JkT1ZOaFU=',
 	'feed',
 	'#data-ad'
 );
